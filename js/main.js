275f9b1698fa6fc9b607dfa054adb8976a9e6784
Andrus1217 <acaldup@gmail.com>
2023-04-25 18:52:50 -0500

    Mi primero commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// alert("Hola mundo :)");
+
+document.write('<h1>Ejemplo desde el JS</h1>')
+document.write('<h1>Ejemplo desde el JS</h1>')
+document.write('<h1>Ejemplo desde el JS</h1>')
+document.write('<h1>Ejemplo desde el JS</h1>')
+document.write('<h1>Ejemplo desde el JS</h1>')
+document.write('<h1>Ejemplo desde el JS</h1>');
+document.write('<h1>Ejemplo desde el JS</h1>');
+document.write('<h2>Ejemplo desde el JS</h2>');
