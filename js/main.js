1f21538ae65db631a26e9bccee0489797a0087dd
Andrus1217 <acaldup@gmail.com>
2023-04-25 19:10:34 -0500

    Cambio de archivo de javascript, se elimina lo anteior

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-// alert("Hola mundo :)");
-
-document.write('<h1>Ejemplo desde el JS</h1>')
-document.write('<h1>Ejemplo desde el JS</h1>')
-document.write('<h1>Ejemplo desde el JS</h1>')
-document.write('<h1>Ejemplo desde el JS</h1>')
-document.write('<h1>Ejemplo desde el JS</h1>')
-document.write('<h1>Ejemplo desde el JS</h1>');
-document.write('<h1>Ejemplo desde el JS</h1>');
-document.write('<h2>Ejemplo desde el JS</h2>');
+alert("dsdfs ds")
